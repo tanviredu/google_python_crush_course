85a4d99652f619c999e6dca11508031f79315629
Tanvir Rahman <laravellearningtanvir@gmail.com>
2020-03-08 09:47:17 -0700

    'added'

---
+++ module4_1.py
@@ -0,0 +1,27 @@
+def format_address(address_string):
+  # Declare variables
+
+  # Separate the address string into parts
+  address = address_string.split()
+  number = address[0]
+  road = " ".join(address[1:])
+
+  # Traverse through the address parts
+  #for __:
+    # Determine if the address part is the
+    # house number or part of the street name
+
+  # Does anything else need to be done 
+  # before returning the result?
+  
+  # Return the formatted string  
+  return "house number {} on street named {}".format(number,road)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
