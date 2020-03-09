4c893a5a11fdfd0b87f991bb37e7937b4bbe442c
Tanvir Rahman <laravellearningtanvir@gmail.com>
2020-03-09 10:32:26 -0700

    'added'

---
+++ ongoing.py
@@ -0,0 +1,26 @@
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+
+
+import csv
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    reader_obj = csv.reader(file_contents)
+    file_contents.split(','))
+    #wordcloud
+    #cloud = wordcloud.WordCloud()
+    #cloud.generate_from_frequencies()
+    #return cloud.to_array()
+calculate_frequencies(file_contents)
